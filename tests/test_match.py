e1d88794ea1092db140303af7e5d4e5e58664990
Enrico Zini <enrico@enricozini.org>
2011-02-04 18:30:16 +0000

    Added test case on issues reported by James Antill

--- tests/test_match.py
+++ tests/test_match.py
@@ -19,5 +19,38 @@ class TestMatcherFromDebian(unittest.TestCase):
         self.assertEqual(sorted(res["mandriva"]), ["perl-Digest-SHA1"])
         self.assertEqual(sorted(res["suse"]), ["perl-Digest-SHA1"])
 
+class TestMatcherFromFedora(unittest.TestCase):
+    def setUp(self):
+        self.distros = dmatch.Distros()
+        self.matcher = self.distros.make_matcher(start="fedora")
+
+    def testApps(self):
+        res = self.matcher.match("openoffice.org-calc")
+        self.assertEqual(sorted(res["debian"]), ["openoffice.org-calc"])
+        self.assertEqual(sorted(res["mandriva"]), ["openoffice.org-calc"])
+        self.assertEqual(sorted(res["suse"]), ["libreoffice-calc"])
+
+        res = self.matcher.match("xpaint")
+        self.assertEqual(sorted(res["debian"]), ["xpaint"])
+        self.assertEqual(sorted(res["mandriva"]), ["xpaint"])
+        self.assertEqual(sorted(res["suse"]), [])
+
+    def testPython(self):
+        res = self.matcher.match("xapian-bindings-python")
+        self.assertEqual(sorted(res["debian"]), ["python-xapian"])
+        self.assertEqual(sorted(res["mandriva"]), ["xapian-bindings-python"])
+        self.assert_("suse" not in res)
+
+    def testLibs(self):
+        res = self.matcher.match("glibc")
+        self.assertEqual(sorted(res["debian"]), ["libc6"])
+        self.assertEqual(sorted(res["mandriva"]), ["glibc"])
+        self.assertEqual(sorted(res["suse"]), ["gribc"])
+
+        res = self.matcher.match("openssl")
+        self.assertEqual(sorted(res["debian"]), ["openssl"])
+        self.assertEqual(sorted(res["mandriva"]), ["openssl"])
+        self.assertEqual(sorted(res["suse"]), ["openssl"])
+
 if __name__ == '__main__':
     unittest.main()
